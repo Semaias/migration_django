0d40a22a4ce0a795b48967e0456f32ee004d51dc
Semaias <semaiasrangel@gmail.com>
2021-07-02 14:43:13 -0300

    Ah shit! Here we go again...

--- django-project/DR/ZK/models.py
+++ django-project/DR/ZK/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Na teoria, isso deve servir pra poder salvar localmente e exportar de forma remota.
 
 class companiesKFS (models.Model):
+# aqui vão os campos das ´companies´ banco da kfs
   name                     = models.CharField(max_length=255)
   has_active               = models.SmallIntegerField()
   has_active_contacts      = models.SmallIntegerField()
@@ -11,12 +12,11 @@ class companiesKFS (models.Model):
   number_users             = models.SmallIntegerField()
   old_number               = models.SmallIntegerField()
   relatorio_totalizadores  = models.SmallIntegerField()
-  version                  = models.SmallIntegerField()
-  active                   = models.SmallIntegerField()
+  version                  = models.SmallIntegerField(default=3)
+  active                   = models.SmallIntegerField(default=1)
   integrated_confirmations = models.SmallIntegerField()
-  created                  = models.DateTimeField()
-  modified                 = models.DateTimeField()
-# aqui vão os campos do banco da kfs
+  created                  = models.DateTimeField(auto_now_add=True)
+  modified                 = models.DateTimeField(auto_now=True)
 
 # class numbersKFS (models.Model):
 # #  aqui vão os campos do banco da kfs
