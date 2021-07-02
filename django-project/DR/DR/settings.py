6b64ef20bc07a9249fdb8c9e6dcdcd00e4e6524c
Semaias <semaiasrangel@gmail.com>
2021-07-02 12:16:38 -0300

    Ah shit! Here we go again...

--- django-project/DR/DR/settings.py
+++ django-project/DR/DR/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #OWN
-    'ZK',
+    'DR',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'DR.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': { },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 
     'm3': {
         'ENGINE': 'django.db.backends.mysql',
