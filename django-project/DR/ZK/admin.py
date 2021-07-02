7d90a8f64b0b45a31232599674bfa94327284e8f
Semaias <semaiasrangel@gmail.com>
2021-07-02 14:15:03 -0300

    Ah shit! Here we go again...

--- django-project/DR/ZK/admin.py
+++ django-project/DR/ZK/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import companiesKFS
+
 # Register your models here.
+admin.site.register(companiesKFS)
--- django-project/DR/ZK/models.py
+++ django-project/DR/ZK/models.py
@@ -4,25 +4,37 @@ from django.db import models
 # Na teoria, isso deve servir pra poder salvar localmente e exportar de forma remota.
 
 class companiesKFS (models.Model):
+  name                     = models.CharField(max_length=255)
+  has_active               = models.SmallIntegerField()
+  has_active_contacts      = models.SmallIntegerField()
+  has_satisfacao           = models.SmallIntegerField()
+  number_users             = models.SmallIntegerField()
+  old_number               = models.SmallIntegerField()
+  relatorio_totalizadores  = models.SmallIntegerField()
+  version                  = models.SmallIntegerField()
+  active                   = models.SmallIntegerField()
+  integrated_confirmations = models.SmallIntegerField()
+  created                  = models.DateTimeField()
+  modified                 = models.DateTimeField()
 # aqui vão os campos do banco da kfs
 
-class numbersKFS (models.Model):
-#  aqui vão os campos do banco da kfs
+# class numbersKFS (models.Model):
+# #  aqui vão os campos do banco da kfs
 
-class callsKFS (models.Model):
-#  aqui vão os campos do banco da kfs
+# class callsKFS (models.Model):
+# #  aqui vão os campos do banco da kfs
 
-class modules_has_companiesKFS (models.Model):
-#  aqui vão os campos do banco da kfs
+# class modules_has_companiesKFS (models.Model):
+# #  aqui vão os campos do banco da kfs
 
-class quick_answersKFS (models.Model):
-#  aqui vão os campos do banco da kfs
+# class quick_answersKFS (models.Model):
+# #  aqui vão os campos do banco da kfs
 
-class scriptsKFS (models.Model):
-#  aqui vão os campos do banco da kfs
+# class scriptsKFS (models.Model):
+# #  aqui vão os campos do banco da kfs
 
-class usersKFS (models.Model):
-#  aqui vão os campos do banco da kfs
+# class usersKFS (models.Model):
+# #  aqui vão os campos do banco da kfs
 
-class tagsKFS (models.Model):
-#  aqui vão os campos do banco da kfs
+# class tagsKFS (models.Model):
+# #  aqui vão os campos do banco da kfs
